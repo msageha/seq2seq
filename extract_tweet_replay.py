05b7e84a9bccd964de33125be4fbaa6884c4bbb0
Mizuki Sango <msageha@yahoo.co.jp>
2018-02-21 10:26:35 +0900

    add

--- extract_tweet_replay.py
+++ extract_tweet_replay.py
@@ -4,6 +4,10 @@ import time
 import sys
 from requests_oauthlib import OAuth1Session, OAuth1
 import requests
+import re
+
+def remove_account(text):
+    re.sub('@[a-zA-Z_+-0-9]')
 
 def getKeyDic(api_name, jsonPath = './api_key.json'):
     with open(jsonPath, 'r') as f:
@@ -31,9 +35,15 @@ def get_tweet(tweet_id):
         flag = True
     return text, flag
 
-def read_tweet_json(file_path):
+def write_dialog(file_path, dialog):
+    with open(f'tweet_dialog/{file}', 'w') as f:
+        for i in dialog:
+            f.write(f'input: {i[0]}\n')
+            f.write(f'output: {i[1]}\n')
+
+def read_tweet_json(file):
     dialog = []
-    with open(file_path) as f:
+    with open(f'tweet/{file}') as f:
         i = 0
         for line in f:
             i += 1
@@ -42,20 +52,17 @@ def read_tweet_json(file_path):
             tweet = json.loads(line)
             if tweet["in_reply_to_status_id"]:
                 tweet_id = tweet["in_reply_to_status_id"]
-                input_text = tweet['text']
+                input_text = tweet['text'].replace('\n', ' ')
                 output_text, flag = get_tweet(tweet_id)
+                output_text = output_text.replace('\n', ' ')
                 if output_text != '':
                     dialog.append((input_text, output_text))
                 if flag:
                     print(i)
-                    break
+                    write_dialog(file, dialog)
+                    dialog = []
     return dialog
 
 if __name__ == '__main__':
     file = '2017-11-23.txt'
-    dialog = read_tweet_json(f'tweet/{file}')
-    with open(f'tweet_dialog/{file}', 'w') as f:
-        for i in dialog:
-            f.write(f'input: {i[0]}')
-            f.write(f'output: {i[1]}')
-    
+    read_tweet_json(f'{file}')
