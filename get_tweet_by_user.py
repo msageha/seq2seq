997e6dcf17fb37988dfd5147b73baaa15e2390a6
Mizuki Sango <msageha@yahoo.co.jp>
2018-02-21 09:37:13 +0900

    add

--- get_tweet_by_user.py
+++ get_tweet_by_user.py
@@ -16,7 +16,7 @@ def getUserTweet(auth, name, folder):
     # timelines = api.statuses.user_timeline(screen_name=name, count=count)
     # for timeline in timelines:
     #   f.write(timeline['text']+'\n')
-    max_id = 963619750776422399 #指定しないとだめなよう
+    max_id = 963619750776422400 #指定しないとだめなよう
     while True:
       timelines = api.statuses.user_timeline(screen_name=name, count=count, max_id=max_id)
       if timelines == []:
@@ -30,12 +30,37 @@ def getUserTweet(auth, name, folder):
         print(max_id)
         break
 
+def searchUserTweet(auth, name, folder):
+  with open(folder, 'a') as f:
+    count = 100
+    api = Twitter(auth=auth)
+    # timelines = api.statuses.user_timeline(screen_name=name, count=count)
+    # for timeline in timelines:
+    #   f.write(timeline['text']+'\n')
+    max_id = 718085631121821700 #指定しないとだめなよう
+    while True:
+      timelines = api.search.tweets(q=name, count=count, max_id=max_id)
+      if timelines == []:
+          print('None')
+          break
+      timelines = timelines['statuses']
+      if timelines==[]:
+          print(max_id)
+          break
+      max_id = timelines[-1]['id'] - 1
+      for timeline in timelines:
+            f.write(json.dumps(timeline))
+            f.write('\n')
+      if len(timelines) < count:
+        print(max_id)
+        break
 if __name__ == '__main__':
     user_id = '3274075003'
     keyDic = getKeyDic('twitter_sm_nz_gk')
     auth = OAuth(keyDic['ACCESS_TOKEN_KEY'], keyDic['ACCESS_TOKEN_SECRET'], keyDic['CONSUMER_KEY'], keyDic['CONSUMER_SECRET'])
     name = '@ms_rinna'
-    getUserTweet(auth, name, f'./tweet/{name}.txt')
+    # getUserTweet(auth, name, f'./tweet/{name}.txt')
+    searchUserTweet(auth, name, f'./tweet/{name}_search.txt')
 
 """
 @ms_rinna : 3274075003
